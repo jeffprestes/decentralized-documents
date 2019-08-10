17b557a06483fd005a271040130810e2ec23d8fe
Jeff Prestes <jeffprestes@gmail.com>
2019-08-10 17:21:33 -0300

    Updates

---
+++ precache-manifest.d6e8ab724015392f6f6d68e86a18da39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b790af910eee671a78b13794f867a35d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47109744782b55810faa",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e91fa48216f1f329c870",
+    "url": "/static/js/2.d07f4739.chunk.js"
+  },
+  {
+    "revision": "47109744782b55810faa",
+    "url": "/static/js/main.9f435214.chunk.js"
+  },
+  {
+    "revision": "07725d820f222e0d9430",
+    "url": "/static/js/runtime~main.4828ea19.js"
+  }
+]);
